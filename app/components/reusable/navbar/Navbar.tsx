64da948c9c82a7ed6d327addc5e288c797724d72
abedinalways <nion.sk5067@gmail.com>
2026-09-14 17:00:56 +0600

    updated

--- app/components/reusable/navbar/Navbar.tsx
+++ app/components/reusable/navbar/Navbar.tsx
@@ -1,6 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Navbar() {
-  return
-  <div className="bg-primary-background"></div>;
+  return (
+    <div className="bg-background px-[200px] py-12" >
+      <Image src="/image/logo.png" alt="Logo" height={90} width={64} className="object-contain w-fit h-full" />
+      <h3>
+
+      </h3>
+    </div>
+  )
 }
